f0bc7e7cc34c0dcc9a5d267a2c1325e4cb9d74e7
oznaak <motap023@gmail.com>
2026-05-14 12:37:08 +0100

    Update branding from "Gameplay Football" to "Classic Manager" in various files

--- src/systems/graphics/rendering/opengl_renderer3d.cpp
+++ src/systems/graphics/rendering/opengl_renderer3d.cpp
@@ -378,7 +378,7 @@ struct GLfunctions {
 
 //#endif
 
-    window = SDL_CreateWindow("Gameplay Football", SDL_WINDOWPOS_UNDEFINED,
+    window = SDL_CreateWindow("Classic Manager", SDL_WINDOWPOS_UNDEFINED,
                                 SDL_WINDOWPOS_UNDEFINED, width, height,
                                 SDL_WINDOW_OPENGL /* | SDL_RESIZABLE*/ |
                                 (fullscreen ? SDL_WINDOW_FULLSCREEN : 0));
