e982bab4b7fec142cfc707771cf89157d59970f8
oznaak <motap023@gmail.com>
2026-05-15 10:34:08 +0100

    Display manager name in the sidebar instead of static text

--- src/menu/imgui_career.cpp
+++ src/menu/imgui_career.cpp
@@ -621,7 +621,7 @@ static void DrawSidebar(float sideW, float winH) {
   ImGui::SetCursorPos(ImVec2(16.0f, 16.0f));
   PushMgrFont(g_ManagerFontBold);
   ImGui::PushStyleColor(ImGuiCol_Text, kAccent);
-  ImGui::TextUnformatted("Classic Manager");
+  ImGui::TextUnformatted(g_CareerHub.manager.name);
   ImGui::PopStyleColor();
   PopMgrFont(g_ManagerFontBold);
 
